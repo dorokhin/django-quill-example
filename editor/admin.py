7f3998806d9b7ebb5fddeaae31ff3fe99b1ae5fc
Andrew Dorokhin <andrew@dorokhin.moscow>
2019-02-01 17:43:27 +0700

    add ImageAdmin & change template

--- editor/admin.py
+++ editor/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Image
 
 
 class ArticleAdmin(admin.ModelAdmin):
     pass
 
 
+class ImageAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(Image, ImageAdmin)
