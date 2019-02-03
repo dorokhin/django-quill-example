5e2e0dd89bb19326435016a825487c5ff7bcc754
Andrew Dorokhin <andrew@dorokhin.moscow>
2019-02-03 17:47:11 +0700

    Add ImageUpload template

--- myeditor/urls.py
+++ myeditor/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from editor import urls as editor_urls
+from django.conf import settings
+from django.conf.urls import include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(editor_urls, namespace='editor')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
