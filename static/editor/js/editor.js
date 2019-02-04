a3f1c1955cb4451009dda97264aa205ed1d38b9a
Andrew Dorokhin <andrew@dorokhin.moscow>
2019-02-04 08:27:20 +0700

    add imageHandler

--- static/editor/js/editor.js
+++ static/editor/js/editor.js
@@ -1,13 +1,16 @@
+
+
 let quill = new Quill('#editor-container', {
   modules: {
     toolbar: [
       [{ header: [1, 2, false] }],
       ['bold', 'italic', 'underline'],
-      ['image', 'code-block']
+      ['image', 'code-block'],
+
     ]
   },
   placeholder: 'Compose an epic...',
-  theme: 'snow'  // or 'bubble'
+  theme: 'snow',
 });
 
 
@@ -19,3 +22,35 @@ form.onsubmit = function() {
 
     console.log("Submitted", $(form).serialize(), $(form).serializeArray());
 };
+
+var toolbar = quill.getModule('toolbar');
+toolbar.addHandler('image', imageHandler);
+
+
+function imageHandler(a, b) {
+    console.log(a, b);
+    var range = this.quill.getSelection();
+    var that = this;
+
+    var input = document.createElement('input');
+    input.type = 'file';
+
+    input.onchange = e => {
+
+       // getting a hold of the file reference
+       var file = e.target.files[0];
+
+       // setting up the reader
+       var reader = new FileReader();
+       reader.readAsDataURL(file); // this is reading as data url
+
+       // here we tell the reader what to do when it's done reading...
+       reader.onload = readerEvent => {
+          var content = readerEvent.target.result; // this is the content!
+          that.quill.insertEmbed(range.index, 'image', content, Quill.sources.USER);
+       }
+
+    };
+
+    input.click();
+}
